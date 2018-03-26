6fd25d49386228a8d761d2c2d8221ffa625ba227
Tim Zhang <ttimasdf@users.noreply.github.com>
2018-03-26 09:25:41 +0800

    Update fingerprint for OP3

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -6,6 +6,6 @@ MODDIR=${0%/*}
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
 resetprop=$(which resetprop)
-$resetprop -n ro.build.description "OnePlus2-user 6.0.1 MMB29M 15 dev-keys"
-$resetprop -n ro.build.fingerprint "OnePlus/OnePlus2/OnePlus2:6.0.1/MMB29M/1447841200:user/release-keys"
-$resetprop -n ro.build.oemfingerprint "6.0.1/MMB29M/1447841200:user/release-keys"
+$resetprop -n ro.build.description "OnePlus3-user 7.1.1 NMF26F 108 dev-keys"
+$resetprop -n ro.build.fingerprint "OnePlus/OnePlus3/OnePlus3:7.1.1/NMF26F/08081200:user/release-keys"
+$resetprop -n ro.build.oemfingerprint "7.1.1/NMF26F/08081200:user/release-keys"
